ad824a2405cadf848fe9065e5a3e5facacfd6a77
Sean-Coll <c18332623@mytudublin.ie>
2021-03-11 14:39:18 +0000

    Made buttons on the home screen clickable.

--- app/src/main/java/com/example/sign_in_register/MainActivity.java
+++ app/src/main/java/com/example/sign_in_register/MainActivity.java
@@ -3,12 +3,41 @@ package com.example.sign_in_register;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
-public class MainActivity extends AppCompatActivity {
+public class MainActivity extends AppCompatActivity implements View.OnClickListener {
+
+    Button login;
+    Button help;
+    Button text;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        login = findViewById(R.id.Login);
+        help = findViewById(R.id.Help);
+        text = findViewById(R.id.Text_Size);
+    }
+
+    @Override
+    public void onClick(View view) {
+
+        switch(view.getId()) {
+
+            case(R.id.Login): {
+
+            }
+
+            case(R.id.Help): {
+
+            }
+
+            case(R.id.Text_Size): {
+
+            }
+        }
     }
 }
