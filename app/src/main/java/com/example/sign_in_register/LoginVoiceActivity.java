b5373228840538a2c39a1c80b17e3b724cdfc3d8
Sean-Coll <c18332623@mytudublin.ie>
2021-03-12 09:27:56 +0000

    Added: LoginVoiceActivity.java login_voice.xml green_tick.png microphone_icon.png

---
+++ app/src/main/java/com/example/sign_in_register/LoginVoiceActivity.java
@@ -0,0 +1,26 @@
+/* This activity provides speech recognition to allow the user to log in from home. Most of the
+ * functionality for this activity comes from SpeechRecognition.java.
+ * Author: Seán Coll
+ * Created: 12/3/21
+ * Last Edited: 12/3/21
+ */
+
+package com.example.sign_in_register;
+
+import android.os.Bundle;
+import android.view.View;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+public class LoginVoiceActivity extends AppCompatActivity implements View.OnClickListener {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+    }
+
+    @Override
+    public void onClick(View view) {
+
+    }
+}
