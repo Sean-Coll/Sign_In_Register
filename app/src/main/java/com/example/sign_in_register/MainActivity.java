9dd08e3e99679d48b6004870367ce8fb4348a876
Sean-Coll <c18332623@mytudublin.ie>
2021-03-16 10:23:02 +0000

    Added speaker icon for the rest of the options on-screen. Created setUpSoundPool() to allow the SoundPool to be easily set up when the app is resumed.

--- app/src/main/java/com/example/sign_in_register/MainActivity.java
+++ app/src/main/java/com/example/sign_in_register/MainActivity.java
@@ -10,12 +10,10 @@ import android.widget.ImageView;
 
 public class MainActivity extends AppCompatActivity implements View.OnClickListener {
 
-    Button login;
-    Button help;
-    Button text;
-    ImageView loginAudio;
+    Button login, help, text;
+    ImageView loginAudio, textDecreaseAudio, textIncreaseAudio, helpAudio;
     CustomSoundPool custSoundPool;
-    int loginSound, textIncreaseSound, textDecreaseSound, helpSound;
+    int loginSound, textDecreaseSound, textIncreaseSound, helpSound;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -26,18 +24,19 @@ public class MainActivity extends AppCompatActivity implements View.OnClickListe
         help = findViewById(R.id.Help);
         text = findViewById(R.id.Text_Size);
         loginAudio = findViewById(R.id.LoginAudio);
+        textDecreaseAudio = findViewById(R.id.SmallTAudio);
+        textIncreaseAudio = findViewById(R.id.BigTAudio);
+        helpAudio = findViewById(R.id.HelpAudio);
         login.setOnClickListener(this);
         help.setOnClickListener(this);
         text.setOnClickListener(this);
         loginAudio.setOnClickListener(this);
+        textDecreaseAudio.setOnClickListener(this);
+        textIncreaseAudio.setOnClickListener(this);
+        helpAudio.setOnClickListener(this);
 
         // SoundPool creation and loading
-        custSoundPool = new CustomSoundPool();
-        custSoundPool.setCon(this);
-        custSoundPool.initialise();
-
-        loginSound = custSoundPool.load(R.raw.login);
-
+        setUpSoundPool();
     }
 
     @Override
@@ -65,7 +64,41 @@ public class MainActivity extends AppCompatActivity implements View.OnClickListe
 
             case(R.id.LoginAudio): {
                 custSoundPool.play(loginSound);
+                break;
+            }
+
+            case(R.id.SmallTAudio): {
+                custSoundPool.play(textDecreaseSound);
+                break;
+            }
+
+            case(R.id.BigTAudio): {
+                custSoundPool.play(textIncreaseSound);
+                break;
+            }
+
+            case(R.id.HelpAudio): {
+                custSoundPool.play(helpSound);
+                break;
             }
         }
+    }
+
+    // Sets up the SoundPool and loads sounds
+    public void setUpSoundPool() {
+        custSoundPool = new CustomSoundPool();
+        custSoundPool.setCon(this);
+        custSoundPool.initialise();
+
+        loginSound = custSoundPool.load(R.raw.login);
+        textDecreaseSound = custSoundPool.load(R.raw.decrease_text_size);
+        textIncreaseSound = custSoundPool.load(R.raw.increase_text_size);
+        helpSound = custSoundPool.load(R.raw.help);
+    }
+
+    @Override
+    protected void onRestart() {
+        super.onRestart();
+        setUpSoundPool(); // Re-create the SoundPool
     }
 }
