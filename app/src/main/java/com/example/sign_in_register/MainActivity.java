fa66e3683f34593c36bcf2efa73b06366bd437f8
Sean-Coll <c18332623@mytudublin.ie>
2021-03-16 10:54:14 +0000

    Minor layout changes. Fixed a bug where tapping the text size button will play the login sound.

--- app/src/main/java/com/example/sign_in_register/MainActivity.java
+++ app/src/main/java/com/example/sign_in_register/MainActivity.java
@@ -59,7 +59,7 @@ public class MainActivity extends AppCompatActivity implements View.OnClickListe
             }
 
             case(R.id.Text_Size): {
-
+                break;
             }
 
             case(R.id.LoginAudio): {
