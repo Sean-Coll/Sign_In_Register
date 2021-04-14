fc6a520f32632b8093dc4c23d5529a3a207e0759
Sean-Coll <c18332623@mytudublin.ie>
2021-04-14 21:25:22 +0100

    Now an image can be uploaded in emergencyFragment. However, the image is not saved at all and going to the settings fragment will wipe it.

--- app/src/main/java/com/example/sign_in_register/fragments/emergencyFragment.java
+++ app/src/main/java/com/example/sign_in_register/fragments/emergencyFragment.java
@@ -1,9 +1,14 @@
 package com.example.sign_in_register.fragments;
 
+import android.Manifest;
+import android.app.Activity;
+import android.app.Application;
 import android.content.Intent;
+import android.content.pm.PackageManager;
 import android.net.Uri;
 import android.os.Bundle;
 
+import androidx.annotation.NonNull;
 import androidx.annotation.Nullable;
 import androidx.fragment.app.Fragment;
 
@@ -14,6 +19,7 @@ import android.widget.EditText;
 import android.widget.ImageButton;
 import android.widget.ImageView;
 import android.widget.TextView;
+import android.widget.Toast;
 
 import com.example.sign_in_register.R;
 
@@ -25,6 +31,10 @@ public class emergencyFragment extends Fragment {
     int textsize;
     String textstyle;
     ImageView uploadButton;
+    ImageView emergencyImage;
+
+    public static final int PERMISSION_CODE = 1000;
+    public static final int IMAGE_PICK = 2000;
 
     public emergencyFragment() {
         // Required empty public constructor
@@ -69,7 +79,17 @@ public class emergencyFragment extends Fragment {
         uploadButton.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
-
+                // Check if permission has been granted
+                if(getActivity().checkSelfPermission(Manifest.permission.READ_EXTERNAL_STORAGE) ==
+                        PackageManager.PERMISSION_DENIED) {
+                    // If permission not granted, request it
+                    String[] permission = {Manifest.permission.READ_EXTERNAL_STORAGE};
+                    getActivity().requestPermissions(permission, PERMISSION_CODE);
+                }
+                else {
+                    // If permission granted
+                    pickImageFromGallery();
+                }
             }
         });
     }
@@ -80,10 +100,43 @@ public class emergencyFragment extends Fragment {
         title = (TextView)view.findViewById(R.id.Titile);
         phone_num = (TextView)view.findViewById(R.id.Phone_No);
         uploadButton = (ImageView)view.findViewById(R.id.Upload_Button);
+        emergencyImage = (ImageView)view.findViewById(R.id.Emergency_Image);
         textsize = 30;
 //        textstyle = "Time";
 
         title.setTextSize(textsize);
         phone_num.setTextSize(textsize);
+    }
+
+    public void pickImageFromGallery() {
+        // Intent to pick image
+        Intent getImage = new Intent(Intent.ACTION_PICK);
+        getImage.setType("image/*");
+        startActivityForResult(getImage,IMAGE_PICK);
+    }
+
+    // Handle permission during run time
+    @Override
+    public void onRequestPermissionsResult(int requestCode, @NonNull String[] permissions, @NonNull int[] grantResults) {
+        super.onRequestPermissionsResult(requestCode, permissions, grantResults);
+        // If permission is being requested
+        if(requestCode == PERMISSION_CODE) {
+            // If permission has been granted
+            if(grantResults.length > 0 && grantResults[0] == PackageManager.PERMISSION_GRANTED) {
+                pickImageFromGallery();
+            }
+            // If permission denied
+            else {
+                Toast.makeText(getActivity(), "Permission required", Toast.LENGTH_LONG).show();
+            }
+        }
+    }
+
+    @Override
+    public void onActivityResult(int requestCode, int resultCode, @Nullable Intent data) {
+        getActivity();
+        if(resultCode == Activity.RESULT_OK && requestCode == IMAGE_PICK) {
+            emergencyImage.setImageURI(data.getData());
+        }
     }
 }
